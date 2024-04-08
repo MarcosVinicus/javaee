3b467233314dfa2a921c9aad37a9437ed1107ca9
MarcosVinicus <166434426+MarcosVinicus@users.noreply.github.com>
2024-04-08 15:19:40 -0300

    Add files via upload
    Query MySQL (CRUD)

---
+++ Agenda/Dumpdbagenda/dbagenda_contatos.sql
@@ -0,0 +1,53 @@
+-- MySQL dump 10.13  Distrib 8.0.36, for Win64 (x86_64)
+--
+-- Host: localhost    Database: dbagenda
+-- ------------------------------------------------------
+-- Server version	8.0.36
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `contatos`
+--
+
+DROP TABLE IF EXISTS `contatos`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `contatos` (
+  `idcon` int NOT NULL AUTO_INCREMENT,
+  `nome` varchar(50) NOT NULL,
+  `fone` varchar(20) NOT NULL,
+  `email` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`idcon`)
+) ENGINE=InnoDB AUTO_INCREMENT=27 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `contatos`
+--
+
+LOCK TABLES `contatos` WRITE;
+/*!40000 ALTER TABLE `contatos` DISABLE KEYS */;
+INSERT INTO `contatos` VALUES (4,'Ashelley','3432','ashelley@gmail.com'),(12,'Giovane','14991615703','giovene@gmail.com'),(15,'Fábio','10982115722','fabio@gmail.com'),(16,'Jonathan','14996271873','jonathan@gmail.com'),(17,'Vitor Gabriel','14997563722','vitor@gmail.com'),(18,'Miguel','14988030740','miguel@gmail.com'),(19,'Jõao','14949540320','joao@gmail.com'),(20,'Carlos','3423432432','fernandamanduca.alisebrae@gmail.com'),(21,'Carlona','5555555555','carlona@gmail.com'),(22,'Márcio','55555','marcio@gmail.com'),(24,'Teste','33333','646565'),(25,'Xamuel ','',''),(26,'Xamuel','13423432','carlona2@gmail.com');
+/*!40000 ALTER TABLE `contatos` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-04-08 14:21:19
